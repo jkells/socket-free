a1fd761877ae0053bc4972102609df24da8783a2
Jared Kells <jkells@gmail.com>
2013-02-20 23:30:14 +1100

    Set permissions

--- upload-bin.sh
+++ upload-bin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -Q '-SITE CHMOD 644 SocketFree.exe' -T ./src/bin/Debug/SocketFree.exe -u $DEPLOY_CRED ftp://vps2.kared.net
+curl -T ./src/bin/Debug/SocketFree.exe -u $DEPLOY_CRED ftp://vps2.kared.net
