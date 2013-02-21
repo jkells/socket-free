8394e8ec11e2e9ba81b3193538be0fdce4e3b58d
Jared Kells <jkells@gmail.com>
2013-02-21 23:43:14 +1100

    encode the equals :)

--- scripts/deploy-keygen.sh
+++ scripts/deploy-keygen.sh
@@ -5,7 +5,8 @@ i=1;
 
 while read -r line 
 do
-  printf 'TRAVIS_SSH_KEY_%03d="%s"\n' $i "$line" 
+  strip_equals=$(echo "$line" | tr '=' '!')
+  printf 'TRAVIS_SSH_KEY_%03d="%s"\n' $i "$strip_equals" 
   i=$[i+1]  
-done < travis_rsa | travis encrypt -s | awk '{print "  - secure: " $0 }'
+done < travis_rsa | travis encrypt -s |awk '{print "  - secure: " $0 }'
 
--- scripts/travis-unpack-ssh-key.sh
+++ scripts/travis-unpack-ssh-key.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Unpacking TRAVIS_SSH_KEY into ~/.ssh/id_rsa"
-env | grep TRAVIS_SSH_KEY | sort | cut -d= -f2 > ~/.ssh/id_rsa
+env | grep TRAVIS_SSH_KEY | sort | cut -d= -f2 | tr '!' '=' > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
